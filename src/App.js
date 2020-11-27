f228ae97fb0f72adada31d0588787aefb89707eb
Isham Bhandari <33259275+ishambhandari@users.noreply.github.com>
2020-11-27 20:39:50 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,12 +17,7 @@ class App extends React.Component{
   constructor(props){
   super(props)
   this.state = {
-    title:"Isham Bhandari",
-    headerLinks: [
-      {title:'Home', path:'/'},
-      {title:'About', path:'/about'},
-      {title:'Contact', path:'/contacts'}
-    ],
+
   
     home: {
       title: "Its going to be legendary",
